46e3be85cfe0d529713deb17c553ec9112940edb
Omkar Hadge <omkar.suresh.hadge@gmail.com>
2022-06-24 20:06:05 +0530

    resolved when city not found

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 let weather = {
     apiKey : "549b0987e7e420b4606ff84114e85f9a",
     fetchWeather: function(city){
-        console.log(city);
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="+
             city +
             "&units=metric&appid="+
             this.apiKey )
             .then((response)=> response.json())
-            .then((data)=> this.displayWeather(data));
+            .then((data)=> this.displayWeather(data))
+            .catch(err => this.displayErr());
+    },
+    displayErr : function(){
+        document.querySelector(".city").innerHTML = "Error... City Not Found";
+        document.querySelector(".loader").style.display="none";
+        document.querySelector(".toHide").style.visibility="visible";
     },
     displayWeather : function(data){
         const { name } =data;
@@ -24,10 +29,10 @@ let weather = {
         document.querySelector(".wind").innerHTML ="Wind Speed : "+ speed +" km/hr";
         document.querySelector(".loader").style.display="none";
         document.querySelector(".weather").style.visibility="visible";
+        document.querySelector(".toHide").style.visibility="visible";
     },
     search : function() {
         this.fetchWeather(document.querySelector(".searchBar").value);
-        console.log(document.querySelector(".searchBar").value);
     }
 };
 
@@ -35,11 +40,13 @@ document.querySelector(".searchIcon").addEventListener("click",function(){
     weather.search();
     document.querySelector(".weather").style.visibility="hidden";
     document.querySelector(".loader").style.display="flex";
+    document.querySelector(".toHide").style.visibility="hidden";
 });
 document.querySelector(".searchBar").addEventListener("keyup",function(e){
     if(e.key=="Enter"){
         weather.search();
         document.querySelector(".weather").style.visibility="hidden";
         document.querySelector(".loader").style.display="flex";
+        document.querySelector(".toHide").style.visibility="hidden";
     }
 });
